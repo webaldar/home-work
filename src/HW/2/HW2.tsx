772505147e9f35b178a53e87f8a6044aa046a54e
webaldar <webaldar@gmail.com>
2024-04-19 15:19:34 +0300

    hw2 finished 2

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -48,7 +48,7 @@ export const HW2 = () => {
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
   const filterUsers = () => {
-    const filteredUsers = users.myFriends.filter(element => element.address.city === 'Los Angeles')
+    const filteredUsers = users.myFriends.filter(user => user.address.city === 'Los Angeles')
    // 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
     setCurrentUsers({ myFriends: filteredUsers });
   };
